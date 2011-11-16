c49dc08cbc4afbc42c19c22696bdadb4e6fa1d6a
Patrick Altman <paltman@gmail.com>
2011-11-16 16:00:23 -0600

    Add pusher support

--- eventlog/models.py
+++ eventlog/models.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
+from django.conf import settings
 from django.db import models
 
 from django.contrib.auth.models import User
 
 from jsonfield.fields import JSONField
+
+
+PUSHER_CONFIG = getattr(settings, "PUSHER_CONFIG", None)
 
 
 class Log(models.Model):
@@ -13,10 +17,27 @@ class Log(models.Model):
     timestamp = models.DateTimeField(default=datetime.now)
     action = models.CharField(max_length=50)
     extra = JSONField(default="{}")
+    
+    class Meta:
+        ordering = ["-timestamp"]
 
 
 def log(user, action, extra):
     if (user is not None and not user.is_authenticated()):
         user = None
+    
+    if PUSHER_CONFIG:
+        import pusher
+        p = pusher.Pusher(
+            app_id=PUSHER_CONFIG["app_id"],
+            key=PUSHER_CONFIG["key"],
+            secret=PUSHER_CONFIG["secret"]
+        )
+        # send utc date (http://stackoverflow.com/questions/948532/how-do-you-convert-a-javascript-date-to-utc/951417#951417)
+        p["event_log"].trigger(action, {
+            "user": user.username,
+            "extra": extra,
+            "date": datetime.utcnow().isoformat()
+        })
     
     return Log.objects.create(user=user, action=action, extra=extra)
