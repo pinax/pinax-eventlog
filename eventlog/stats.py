a81fbdd334dc475554e77bbb71ae00985f2d23c4
Patrick Altman <paltman@gmail.com>
2012-01-12 11:16:37 -0500

    Add active_seven and active_thirty users

--- eventlog/stats.py
+++ eventlog/stats.py
@@ -3,8 +3,27 @@ from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 
 
+def used_active(days):
+    used = User.objects.filter(
+        log__timestamp__gt=datetime.now() - timedelta(days=days)
+    ).distinct().count()
+    
+    active = User.objects.filter(
+        log__timestamp__gt=datetime.now() - timedelta(days=days)
+    ).exclude(
+        date_joined__gt=datetime.now() - timedelta(days=days)
+    ).distinct().count()
+    
+    return used, active
+
+
 def stats():
+    used_seven, active_seven = used_active(7)
+    used_thirty, active_thirty = used_active(30)
+    
     return {
-        "used_site_last_thirty_days": User.objects.filter(log__timestamp__gt=datetime.now() - timedelta(days=30)).distinct().count(),
-        "used_site_last_seven_days": User.objects.filter(log__timestamp__gt=datetime.now() - timedelta(days=7)).distinct().count()
+        "used_seven": used_seven,
+        "used_thirty": used_thirty,
+        "active_seven": active_seven,
+        "active_thirty": active_thirty
     }
