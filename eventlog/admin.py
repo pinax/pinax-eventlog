9a33f048c99bb60e0026a0fe27d6a7fb60da3a9e
Patrick Altman <paltman@gmail.com>
2013-07-06 10:56:37 -0500

    Reduce query count with select_related

--- eventlog/admin.py
+++ eventlog/admin.py
@@ -4,6 +4,14 @@ from eventlog.models import Log
 
 
 class LogAdmin(admin.ModelAdmin):
+    
+    def queryset(self, request):
+        qs = super(LogAdmin, self).queryset(request)
+        return qs.select_related("user")
+    
+    def get_queryset(self, request):
+        return self.queryset(request)
+    
     raw_id_fields = ["user"]
     list_filter = ["action", "timestamp"]
     list_display = ["timestamp", "user", "action", "extra"]
