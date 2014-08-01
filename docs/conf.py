52f178bdbdf9cfba8c5d83f7f706aa89b10d883b
James Tauber <jtauber@jtauber.com>
2014-08-01 00:09:05 -0400

    flake8 compliance

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-import sys, os
+import os
+import sys
 
 extensions = []
 templates_path = []
@@ -12,8 +13,8 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 htmlhelp_basename = '%sdoc' % project
 latex_documents = [
-  ('index', '%s.tex' % project, u'%s Documentation' % project,
-   copyright_holder, 'manual'),
+    ('index', '%s.tex' % project, u'%s Documentation' % project,
+     copyright_holder, 'manual'),
 ]
 man_pages = [
     ('index', project, u'%s Documentation' % project,
