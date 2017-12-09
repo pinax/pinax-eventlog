4952c7f60d73ea418d602008b4f6cb3ca7e3b929
Patrick Altman <paltman@gmail.com>
2017-12-09 08:54:52 -0600

    2.0.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "1.1.2"
+VERSION = "2.0.0"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-eventlog.svg
     :target: https://pypi.python.org/pypi/pinax-eventlog/
